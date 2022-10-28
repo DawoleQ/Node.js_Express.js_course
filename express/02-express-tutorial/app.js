a9ae52ab3e760e2f0f9cc39fe27a044a015e70c8
Dawid Gardzielewski <thelastcinnamonbun@gmail.com>
2022-10-28 16:32:30 +0200

    commit 28.10.2022

--- express/02-express-tutorial/app.js
+++ express/02-express-tutorial/app.js
@@ -1,49 +1,20 @@
-const http = require('http')
-const {readFileSync} = require('fs')
+const express = require('express')
+const path = require('path')
+const app = express()
 
-// get all files
-const homePage = readFileSync('./navbar-app/index.html')
-const homeStyle = readFileSync('./navbar-app/styles.css')
-const homeImage = readFileSync('./navbar-app/logo.svg')
-const homeLogic = readFileSync('./navbar-app/browser-app.js')
+// setup static and middleware
+app.use(express.static('./public'))
 
-const server = http.createServer((req, res) => {
-    // console.log(req.method);
-    // console.log(req.url);
+// app.get('/', (req, res)=>{
+//     res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
+// adding to assets
+// SSR
+// })
 
-    const url = req.url
-    console.log(url);
-    // home page
-    if(url === '/') {
-        res.writeHead(200, {'content-type': 'text/html'})
-        res.write(homePage)
-        res.end() 
-    } 
-    // styles
-    else if(url === '/styles.css') {
-        res.writeHead(200, {'content-type': 'text/css'})
-        res.write(homeStyle)
-        res.end()
-    } 
-    // logo
-    else if(url === '/logo.svg') {
-        res.writeHead(200, {'content-type': 'image/svg+xml'})
-        res.write(homeImage)
-        res.end()
-    } 
-    // logic
-    else if(url === '/browser-app.js') {
-        res.writeHead(200, {'content-type': 'text/javascript'})
-        res.write(homeLogic)
-        res.end()
-    } 
-    // 404
-    else {
-        res.writeHead(404, {'content-type': 'text/html'})
-        res.write('<h1>404: Not Found :(</h1>')
-        res.end()
-    }
-    
+app.all('*', (req, res)=>{
+    res.status(404).send('<h1>404: Not Found :(</h1>')
 })
 
-server.listen(5000)
+app.listen(5000, ()=>{
+    console.log('Server listening on port 5000...')
+})
