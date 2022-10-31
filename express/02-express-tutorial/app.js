7d9f3ec19f7d369df7e68edb4cb6512b8a3bf1e1
Dawid Gardzielewski <thelastcinnamonbun@gmail.com>
2022-10-31 14:41:32 +0100

    commit 31.10.2022

--- express/02-express-tutorial/app.js
+++ express/02-express-tutorial/app.js
@@ -1,20 +1,33 @@
 const express = require('express')
-const path = require('path')
 const app = express()
+const {products} = require('./data')
 
-// setup static and middleware
-app.use(express.static('./public'))
+app.get('/', (req, res) => {
+    res.send('<h1> Home Page </h1> <a href="/api/products">products</a>')
+})
 
-// app.get('/', (req, res)=>{
-//     res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
-// adding to assets
-// SSR
-// })
+app.get('/api/products', (req, res) => {
+    const newProducts = products.map((product) => {
+        const {id, name, image} = product
+        return {id, name, image}
+    })
+    res.json(newProducts)
+})
 
-app.all('*', (req, res)=>{
-    res.status(404).send('<h1>404: Not Found :(</h1>')
+app.get('/api/products/:productID', (req, res) => {
+    const {productID} = req.params
+    const singleProduct = products.find((product) => product.id == productID)
+    if(!singleProduct) {
+        return res.status(404).send(`<h1>404: Not Found :(</h1> </br> <p>A product with id: "${productID}" does not exist.</p>`)
+    }
+    return res.json(singleProduct)
+})
+
+app.get('/api/products/:productID/reviews/:review', (req, res) => {
+    console.log(req.params)
+    res.send('hello world')
 })
 
 app.listen(5000, ()=>{
-    console.log('Server listening on port 5000...')
+    console.log('Server listening on port 5000...');
 })
