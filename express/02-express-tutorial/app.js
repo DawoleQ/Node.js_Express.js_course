9a2b1217512e2270ab0e49f6589bd82126b237f0
Dawid Gardzielewski <thelastcinnamonbun@gmail.com>
2022-11-08 21:13:36 +0100

    commit 08.11.2022

--- express/02-express-tutorial/app.js
+++ express/02-express-tutorial/app.js
@@ -28,6 +28,26 @@ app.get('/api/products/:productID/reviews/:review', (req, res) => {
     res.send('hello world')
 })
 
+app.get('/api/v1/query', (req, res) => {
+    console.log(req.query)
+    const {search, limit} = req.query
+    let sortedProducts = [...products]
+
+    if(search) {
+        sortedProducts = sortedProducts.filter((product) => {
+            return product.name.startsWith(search)
+        })
+    }
+    if(limit) {
+        sortedProducts = sortedProducts.slice(0, Number(limit))
+    }
+    if(sortedProducts.length < 1) {
+        return res.status(200).json({sucess: true, data: []})
+    }
+
+    res.status(200).json(sortedProducts)
+})
+
 app.listen(5000, ()=>{
     console.log('Server listening on port 5000...');
 })
